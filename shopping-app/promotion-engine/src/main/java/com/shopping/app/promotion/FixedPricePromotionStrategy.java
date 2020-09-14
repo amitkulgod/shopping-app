2ba4f4008dceea06835f00d7ae01d4e6bb254a9b
Kulgod <akulgod@apac.corpdir.net>
2020-09-14 19:38:49 +0530

    promotion stratergy classes created

---
+++ shopping-app/promotion-engine/src/main/java/com/shopping/app/promotion/FixedPricePromotionStrategy.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.shopping.app.promotion;
+
+import java.util.Map;
+
+import com.shopping.app.product.catalogue.Product;
+
+/**
+ * 
+ * @author AKULGOD
+ *
+ */
+public class FixedPricePromotionStrategy implements Promotion {
+
+	@Override
+	public Integer apply(Map<Product, Integer> product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ shopping-app/promotion-engine/src/main/java/com/shopping/app/promotion/QuantityPromotionStrategy.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.shopping.app.promotion;
+
+import java.util.Map;
+
+import com.shopping.app.product.catalogue.Product;
+
+/**
+ * @author AKULGOD
+ *
+ */
+public class QuantityPromotionStrategy implements Promotion{
+
+	@Override
+	public Integer apply(Map<Product, Integer> product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
