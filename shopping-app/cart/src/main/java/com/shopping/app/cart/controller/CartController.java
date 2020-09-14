2b94ed23bfa2d8eb84a895ebdf875269ca46479a
AKULGOD <AKULGOD@CGSCLXI48981177.inedc.corpintra.net>
2020-09-14 20:42:46 +0530

    Cart and Cart controller created

--- shopping-app/cart/src/main/java/com/shopping/app/cart/controller/CartController.java
+++ shopping-app/cart/src/main/java/com/shopping/app/cart/controller/CartController.java
@@ -1,5 +1,134 @@
+/**
+ * 
+ */
 package com.shopping.app.cart.controller;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.shopping.app.cart.entity.Cart;
+import com.shopping.app.cart.entity.Products;
+import com.shopping.app.product.catalogue.Product;
+import com.shopping.app.promotion.FixedPricePromotionStrategy;
+import com.shopping.app.promotion.QuantityPromotionStrategy;
+
+/**
+ * @author AKULGOD
+ *
+ */
 public class CartController {
+
+	Cart cart = new Cart();
+
+	private int ch = 0;
+
+	public CartController() {
+		menu();
+	}
+
+	public void startScreen() {
+		System.out.println("\n ** Shopping Application  **");
+		System.out.println("\n Please select the options below to proceed further");
+		System.out.println("1. Display All Products");
+		System.out.println("2. Display Cart");
+		System.out.println("0. Exit\n");
+	}
+
+	public void cartMenu() {
+		System.out.println("\n Please select the options below to proceed further");
+		System.out.println("1. Add to Cart");
+		System.out.println("2. Remove From Cart");
+		System.out.println("3. Check out");
+	}
+
+	public void menu() {
+		startScreen();
+		getUserInput();
+
+		switch (ch) {
+		case 1:
+			displayAllProducts();
+			cartMenu();
+			getUserInput();
+			mainChoice1();
+			break;
+		case 2:
+			showCart();
+			break;
+		case 0:
+			System.exit(0);
+			break;
+		default:
+
+			break;
+		}
+
+	}
+
+	private void mainChoice1() {
+			switch (ch) {
+			case 1:
+				addProductToCart();
+				showCart();
+				cartMenu();
+				getUserInput();
+				mainChoice1();
+			break;
+			case 2:
+				removeProductFromCart();
+				showCart();
+				cartMenu();
+				getUserInput();
+				mainChoice1();
+				break;
+				
+			case 3:
+				System.exit(0);
+				break;	
+			default:
+
+				break;
+			}
+
+
+	}
+
+	private int getUserInput() throws NumberFormatException {
+		Scanner in = new Scanner(System.in);
+		ch = Integer.parseInt(in.nextLine());
+		return ch;
+	}
+
+	private void displayAllProducts() {
+		List<Product> products = new Products().getProducts();
+		System.out.println("SKU ID" + " - " + "PRODUCT NAME" + " -    " + "PRICE");
+		for (Product prod : products) {
+			System.out.println(prod.getSkuId() + "      -    " + prod.getName() + "         -    " + prod.getPrice());
+		}
+	}
+
+	private void addProductToCart() {
+		System.out.println("Enter Product SKU ID :");
+		int skuId = getUserInput();
+		System.out.println("Enter Qunatity :");
+		int quantity = getUserInput();
+		cart.addProductToCartBySKUID(skuId, quantity);
+	}
+
+	private void showCart() {
+		cart.printCartItems();
+		int afterDiscount=cart.applyPromotion(new QuantityPromotionStrategy())+cart.applyPromotion(new FixedPricePromotionStrategy());
+		System.out.println("Total Amount after applying promotions  is -" + afterDiscount );
+		
+
+	}
+
+	private void removeProductFromCart() {
+		System.out.println(" Enter the Product SKU ID  to be removed :");
+		int skuId = getUserInput();
+		cart.removeProductBySKUID(skuId);
+		
+
+	}
 
 }
---
+++ shopping-app/cart/src/main/java/com/shopping/app/cart/entity/Cart.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.shopping.app.cart.entity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.shopping.app.product.catalogue.Product;
+import com.shopping.app.promotion.Promotion;
+
+/**
+ * @author AK
+ *
+ */
+public class Cart {
+
+	Map<Product, Integer> cartItems = new HashMap<>();
+
+	public void addProductToCartBySKUID(int pid, int quantity) {
+		Product product = getProductBySKUID(pid);
+		addToCart(product, quantity);
+	}
+
+	private Product getProductBySKUID(int skuId) {
+		Product product = null;
+		List<Product> products = new Products().getProducts();
+		for (Product prod : products) {
+			if (prod.getSkuId() == skuId) {
+				product = prod;
+				break;
+			}
+		}
+		return product;
+	}
+
+	private void addToCart(Product product, Integer quantity) {
+		cartItems.put(product, quantity);
+	}
+
+	public void removeProductBySKUID(int skuId) {
+
+		if (!cartItems.isEmpty()) {
+			Product prod = getProductBySKUID(skuId);
+			cartItems.remove(prod);
+
+		} else {
+			System.out.println("\n Cart is empty !");
+		}
+	}
+
+	
+	
+	
+	
+	public void printCartItems() {
+	
+		if (!cartItems.isEmpty()) {
+			for (Entry<Product, Integer> entry : cartItems.entrySet()) {
+				System.out.println("Product : Qty - " + entry.getKey() + ":" + entry.getValue());
+			}
+			System.out.println("Total Amount is -" + calculateTotal());
+		} else {
+			System.out.println("\n Cart is empty !");
+		}
+
+	}
+
+	public int calculateTotal() {
+		int sum = 0;
+		for (Entry<Product, Integer> entry : cartItems.entrySet()) {
+			sum += entry.getKey().getPrice() * entry.getValue();
+
+		}
+		return sum;
+	}
+	
+	
+	public Integer applyPromotion(Promotion promotion){
+		System.out.println("\n ** Active promtions ** ");
+		System.out.println("\n Buy 3 Product A for 130");
+		System.out.println("\n Buy 2 Product B for 45");
+		System.out.println("\n Buy Product C & D 30");
+		
+		return promotion.apply(cartItems);
+	}
+	
+	
+	
+
+}
