3532804fd22cd4a5b25c1e11bcabfe9dd1313638
AKULGOD <AKULGOD@CGSCLXI48981177.inedc.corpintra.net>
2020-09-14 22:21:43 +0530

    Unit test cases added for Quantity promotion strategy classes

--- shopping-app/promotion-engine/src/test/java/com/shopping/app/promotion/test/TestQuantityPromotionStrategy.java
+++ shopping-app/promotion-engine/src/test/java/com/shopping/app/promotion/test/TestQuantityPromotionStrategy.java
@@ -5,6 +5,9 @@ package com.shopping.app.promotion.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +16,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.shopping.app.product.catalogue.Product;
 import com.shopping.app.promotion.QuantityPromotionStrategy;
 
 /**
@@ -43,9 +47,60 @@ public class TestQuantityPromotionStrategy {
 	}
 	
 	@Test
-	public void test_promotionforA_() {
-		qps.apply(null);
+	public void test_promotionforA_WhenNonOfferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product a=new Product(1, "A", 50.0D);
+		productsMap.put(a,1);
+		assertTrue((a.getPrice()*1) == qps.apply(productsMap));
 	}
+	
+	@Test
+	public void test_promotionforA_WhenOfferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product a=new Product(1, "A", 50.0D);
+		productsMap.put(a,3);
+		assertTrue((130) == qps.apply(productsMap));
+	}
+	
+	@Test
+	public void test_promotionforA_WhenOfferAndNonofferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product a=new Product(1, "A", 50.0D);
+		productsMap.put(a,5);
+		assertTrue(((130)+(2*50)) == qps.apply(productsMap));
+	}
+	
+	
+	@Test
+	public void test_promotionforB_WhenNonOfferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product b=new Product(2, "B", 30.0D);
+		productsMap.put(b,1);
+		assertTrue((b.getPrice()*1) == qps.apply(productsMap));
+	}
+	
+	@Test
+	public void test_promotionforB_WhenOfferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product b=new Product(2, "B", 30.0D);
+		productsMap.put(b,2);
+		assertTrue((45) == qps.apply(productsMap));
+	}
+	
+	@Test
+	public void test_promotionforB_WhenOfferAndNonofferQuantity() {
+		
+		Map<Product, Integer> productsMap=new HashMap<>();
+		Product b=new Product(2, "B", 30.0D);
+		productsMap.put(b,3);
+		assertTrue(((45)+(1*30)) == qps.apply(productsMap));
+	}
+	
 	
 	
 	@Test
